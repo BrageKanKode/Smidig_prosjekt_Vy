f363598b61a6ecbb149b06609721c3620293fefd
Brage Fridriksson <bragge@live.no>
2020-05-13 13:27:57 +0200

    added activities to miljopoeng links

---
+++ app/src/main/java/com/example/leafly_application_git/activities/UsePointsActivity.kt
@@ -0,0 +1,15 @@
+package com.example.leafly_application_git.activities
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.leafly_application_git.R
+import kotlinx.android.synthetic.main.activity_travel_details.*
+
+class UsePointsActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?)  {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_use_points)
+    }
+}
