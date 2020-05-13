1a6bbae7529333eaf17cfab2a6686cef1c1ef22f
Brage Fridriksson <bragge@live.no>
2020-05-13 09:53:53 +0200

    added payment activity

---
+++ app/src/main/java/com/example/leafly_application_git/activities/PaymentActivity.kt
@@ -0,0 +1,14 @@
+package com.example.leafly_application_git.activities
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.leafly_application_git.R
+
+class PaymentActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_payment)
+
+    }
+}
--- app/src/main/java/com/example/leafly_application_git/fragments/search/SearchFragment.kt
+++ app/src/main/java/com/example/leafly_application_git/fragments/search/SearchFragment.kt
@@ -1,5 +1,6 @@
 package com.example.leafly_application_git.fragments.search
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -9,6 +10,8 @@ import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
 import com.example.leafly_application_git.R
+import com.example.leafly_application_git.activities.PaymentActivity
+import kotlinx.android.synthetic.main.fragment_search.view.*
 
 class SearchFragment : Fragment() {
 
@@ -24,6 +27,12 @@ class SearchFragment : Fragment() {
         searchViewModel.text.observe(viewLifecycleOwner, Observer {
             textView.text = it
         })
+
+        root.btnVidere.setOnClickListener {
+            requireActivity().startActivity(
+                Intent(requireActivity(), PaymentActivity::class.java)
+            )
+        }
 
         return root
     }
