07c9069bd9bfce8d4c804b5c10f83d128dfdcd5f
joakimstolen <joakimstoelen@hotmail.com>
2020-05-19 09:52:41 +0200

    Added functionality that increments progression when a dummy-button is clicked

--- app/src/main/java/com/example/leafly_application_git/fragments/main_page/miljopoints/MiljopointsFragment.kt
+++ app/src/main/java/com/example/leafly_application_git/fragments/main_page/miljopoints/MiljopointsFragment.kt
@@ -1,10 +1,12 @@
 package com.example.leafly_application_git.fragments.main_page.miljopoints
 
+import android.app.ProgressDialog
 import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ProgressBar
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProviders
@@ -13,18 +15,26 @@ import com.example.leafly_application_git.activities.miljopoints.MembershipBenef
 import com.example.leafly_application_git.activities.miljopoints.progression.ProgressionActivity
 import com.example.leafly_application_git.activities.miljopoints.usePoints.UsePointsActivity
 import com.google.zxing.integration.android.IntentIntegrator
+import kotlinx.android.synthetic.main.fragment_miljopoints.*
 import kotlinx.android.synthetic.main.fragment_miljopoints.view.*
+import kotlinx.android.synthetic.main.fragment_miljopoints.view.progressBar
 
 
 class MiljopointsFragment : Fragment() {
 
     private lateinit var miljopointsViewModel: MiljopointsViewModel
 
+
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
 
         miljopointsViewModel = ViewModelProviders.of(this).get(MiljopointsViewModel::class.java)
 
         val root = inflater.inflate(R.layout.fragment_miljopoints, container, false)
+
+
+        root.incrementProgressionBtn.setOnClickListener {
+            progressBar.incrementProgressBy(3)
+        }
 
         //Shows actionbar
         (activity as AppCompatActivity?)!!.supportActionBar!!.show()
@@ -58,6 +68,7 @@ class MiljopointsFragment : Fragment() {
 
         return root
     }
+
 
     fun scanFromFragment() {
         IntentIntegrator.forSupportFragment(this).initiateScan();
