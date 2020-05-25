0ca6bee771127521f7eafdb25f0f15d53719b4f7
jonerikrotnebo <jonrotnebo1@gmail.com>
2020-05-25 14:15:25 +0200

    changed recyclerview in use for travel detail *changes to design for selectTimeActivity *changes to travelDetailActivity *added Drawables *changed strings and added more.

--- app/src/main/java/com/example/leafly_application_git/activities/search/trip/SelectTimeActivity.kt
+++ app/src/main/java/com/example/leafly_application_git/activities/search/trip/SelectTimeActivity.kt
@@ -7,7 +7,7 @@ import com.example.leafly_application_git.R
 import com.example.leafly_application_git.activities.search.CustomViewHolder
 import com.example.leafly_application_git.data.Json
 import com.google.gson.GsonBuilder
-import kotlinx.android.synthetic.main.activity_select_time.*
+import kotlinx.android.synthetic.main.activity_confirmation_recycler.*
 import java.io.InputStream
 
 @Suppress("NULLABILITY_MISMATCH_BASED_ON_JAVA_ANNOTATIONS")
@@ -17,7 +17,7 @@ class SelectTimeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        setContentView(R.layout.activity_select_time)
+        setContentView(R.layout.activity_confirmation_recycler)
 
         val from: String = intent.getStringExtra(CustomViewHolder.FROM_LOCATION_KEY)
         val to: String = intent.getStringExtra(CustomViewHolder.TO_LOCATION_KEY)
@@ -25,7 +25,7 @@ class SelectTimeActivity : AppCompatActivity() {
         val points: String = intent.getStringExtra(CustomViewHolder.MILJO_POENG_KEY)
         fetchJson(from, to, price, points)
 
-        recyclerView_time_select.layoutManager = LinearLayoutManager(this)
+        recycler_view_confirmation.layoutManager = LinearLayoutManager(this)
 
     }
 
@@ -42,10 +42,10 @@ class SelectTimeActivity : AppCompatActivity() {
         val time = gson.fromJson(json, Json::class.java)
 
         //RecyclerView_main.adapter = SelectTravelAdapter(location)
-        recyclerView_time_select.adapter = ChooseTravelAdapter(time, from,to,price,points)
+        recycler_view_confirmation.adapter = ChooseTravelAdapter(time, from,to,price,points)
 
         runOnUiThread {
-            recyclerView_time_select.adapter =
+            recycler_view_confirmation.adapter =
                 ChooseTravelAdapter(
                     time, to, from, price, points
                 )
--- app/src/main/java/com/example/leafly_application_git/activities/search/trip/TravelDetailActivity.kt
+++ app/src/main/java/com/example/leafly_application_git/activities/search/trip/TravelDetailActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.leafly_application_git.R
+import kotlinx.android.synthetic.main.activity_confirmation_recycler.*
 import kotlinx.android.synthetic.main.activity_select_time.*
 
 @Suppress("NULLABILITY_MISMATCH_BASED_ON_JAVA_ANNOTATIONS")
@@ -12,7 +13,7 @@ class TravelDetailActivity : AppCompatActivity(){
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        setContentView(R.layout.activity_select_time)
+        setContentView(R.layout.activity_confirmation_recycler)
 
         val departure: String = intent.getStringExtra(Custom.DEPARTURE_KEY)
         val arrival: String = intent.getStringExtra(Custom.ARRIVAL_KEY)
@@ -22,7 +23,7 @@ class TravelDetailActivity : AppCompatActivity(){
         val points: String = intent.getStringExtra(Custom.DETAILS_POINTS_KEY)
         dataPassClass(departure,arrival,departureTime,arrivalTime,price,points)
 
-        recyclerView_time_select.layoutManager = LinearLayoutManager(this)
+        recycler_view_confirmation.layoutManager = LinearLayoutManager(this)
     }
 
     private fun dataPassClass(departure: String,
@@ -32,10 +33,10 @@ class TravelDetailActivity : AppCompatActivity(){
                               price: String,
                               points: String){
 
-        recyclerView_time_select.adapter = TravelDetailAdapter(departure, arrival, departureTime, arrivalTime, price, points)
+        recycler_view_confirmation.adapter = TravelDetailAdapter(departure, arrival, departureTime, arrivalTime, price, points)
 
         runOnUiThread{
-            recyclerView_time_select.adapter =
+            recycler_view_confirmation.adapter =
                 TravelDetailAdapter(departure, arrival, departureTime, arrivalTime, price, points)
         }
     }
