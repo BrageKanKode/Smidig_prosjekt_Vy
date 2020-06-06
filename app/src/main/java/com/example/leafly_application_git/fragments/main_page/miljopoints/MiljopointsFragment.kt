b29fa8129f7965f1167d60609159c1277ed23532
alice clausen <alice.clausen@gmail.com>
2020-06-06 13:11:51 +0200

    UI miljopoeng startpage

--- app/src/main/java/com/example/leafly_application_git/fragments/main_page/miljopoints/MiljopointsFragment.kt
+++ app/src/main/java/com/example/leafly_application_git/fragments/main_page/miljopoints/MiljopointsFragment.kt
@@ -311,19 +311,19 @@ class MiljopointsFragment : Fragment() {
                 if (user?.level!! == 1) {
                     textView_level_points.text = "Frø"
                     val froDrawable = R.drawable.ic_fro_membership
-                    imageView_display_level_icon.setImageResource(froDrawable)
+                    //imageView_display_level_icon.setImageResource(froDrawable)
                 }
 
                 if (user?.level!! == 2) {
                     textView_level_points.text = "Spire"
                     val spireDrawable = R.drawable.ic_spire_membership
-                    imageView_display_level_icon.setImageResource(spireDrawable)
+                    //imageView_display_level_icon.setImageResource(spireDrawable)
                 }
 
                 if (user?.level!! == 3) {
                     textView_level_points.text = "Tre"
                     val treDrawable = R.drawable.ic_tre_membership
-                    imageView_display_level_icon.setImageResource(treDrawable)
+                    //imageView_display_level_icon.setImageResource(treDrawable)
                 }
 
 
